80c8809266f2802f78a0ae6b114282d831dc54f8
Cesar R <cejo08@yahoo.com>
2026-04-29 11:32:27 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.end('<h1>Hola mundo</h1>');
+});
+
+const PORT = 3002;
+server.listen(PORT, () => {
+	console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
